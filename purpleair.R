b9c404631e823881ed943282728bbb05d85b7884
colegasn <colegasn@mail.uc.edu>
2023-07-18 16:05:45 -0400

    Removed tutorial information from Introduction section. Added additional URLs to pull archived PAS files from.

--- purpleair.R
+++ purpleair.R
@@ -2,31 +2,11 @@
 ## Author: Stephen Colegate
 ## Last Updated: 7/18/2023
 
-# This is the 'purpleair.R' script that is used with the vignette 'PurpleAir
-# Data Exploration' for the RISE program at Cincinnati Children's Hospital
-# Medical Center. All code from the vignette has been reproduced here.
-# Please run each line of code carefully as you work through the tutorial
-# and explore the data by trying out code of your own.
-
 # Links to the GitHub site and the tutorial itself:
 # GitHub page: https://github.com/geomarker-io/purple_air_data_in_R/tree/main#readme
 # PurpleAir Data Exploration Tutorial: https://geomarker.io/purple_air_data_in_R/#sec-live
 
-# Opening and Using R -----------------------------------------------------
-
-# Assign values
-x <- 2    # assign the letter 'x' the value 2
-y <- 9    # assign the letter 'y' the value 9
-
-# Perform simple operations
-x + y     # equivalent to '2' + '9'
-sqrt(y)   # sqrt = square-root
-
-
 # Saving/Loading Data -----------------------------------------------------
-
-# Check the current working directory
-getwd()
 
 # Install the 'here' package - only run this once
 # install.packages("here")
@@ -34,17 +14,11 @@ getwd()
 # Load the 'here' package
 library(here)
 
+# Set working directory first!
+
 # Save the file path location
 mypath <- here()
 mypath
-
-# Save data frame as a file
-head(mtcars, n=10)
-saveRDS(mtcars, file = here(mypath, "cardata.rds"))
-
-# Load data frame into the R session
-mycars <- readRDS(file = here(mypath, "cardata.rds"))
-head(mycars, n=10)
 
 
 # Packages ----------------------------------------------------------------
@@ -54,42 +28,23 @@ head(mycars, n=10)
 #                    'MazamaCoreUtils', 'MazamaSpatialUtils'))
 
 # Install the 'AirSensor' package - only need to run once
-devtools::install_github("MazamaScience/AirSensor")
+# devtools::install_github("MazamaScience/AirSensor")
 
 # Load required R packages
 library(dplyr)
 library(ggplot2)
-library(here)
 library(MazamaCoreUtils)
 library(MazamaSpatialUtils)
 library(AirSensor)
 
-# Display help documentation for 'mtcars'
-help(mtcars)
-
-# Display first n=10 rows of the example cars data 
-head(mtcars, n=10)
-
-# Reference the 'mpg' column within 'mtcars'
-mileage <- mtcars$mpg
-head(mileage, n=20)
-
 
 # Fetch and Set API Key ---------------------------------------------------
-
-# Replace the text API KEY below with your actual API Key from PurpleAir
-PurpleAir_API = "API KEY"
-# Open new R script, paste the above line in, and save as 'API_KEY.R'
 
 # Read 'API_KEY.R' file containing the API Key
 source(here(mypath, "API_KEY.R"))
 
 # Set the API Key
 setAPIKey(provider = "PurpleAir-read", key = PurpleAir_API)
-
-# Return all API Keys currently set
-getAPIKey()
-showAPIKeys()
 
 
 # Read Live PAS from PurpleAir Dashboard ----------------------------------
@@ -111,27 +66,17 @@ pas <- new_pas(pas_filename = "PAS_Hamilton.rds",
 
 # Read Archived PAS Data --------------------------------------------------
 
-# Set location of pre-generated data files
-setArchiveBaseUrl("https://airfire-data-exports.s3-us-west-2.amazonaws.com/PurpleAir/v1")
+# Set URL location of pre-generated data files
+setArchiveBaseUrl("https://airsensor.aqmd.gov/PurpleAir/v1/")
 
-# Load a PAS object from a specific date (takes a minute to load)
-pas_apr10 <- pas_load(datestamp="20200410")   # '20200410' = '4-10-2020'
-
+# Load a PAS object from a specific date
+pas_apr10 <- pas_load(datestamp="20220410")   # '20220410' = '4-10-2022'
 
 
 # Explore PAS -------------------------------------------------------------
 
 # Example PAS from AirSensor package (can use above PAS file you created here)
 pas <- AirSensor::example_pas
-
-# Open help documentation for example PAS
-help(example_pas)
-
-# Examine the first 10 rows
-print(pas, n=10, max_footer_lines=0)
-
-# Get list of column names
-names(pas)
 
 # Take a look at only PM2.5 data
 pm25 <- pas %>%
@@ -155,14 +100,8 @@ pas %>%
 #####
 
 # Example using the archived PAS (not shown in tutorial)
-setArchiveBaseUrl("https://airfire-data-exports.s3-us-west-2.amazonaws.com/PurpleAir/v1")
-pas_apr10 <- pas_load(datestamp="20200410")   # '20200410' = '4-10-2020'
-
-# Examine the first 10 rows
-print(pas_apr10, n=10, max_footer_lines=0)
-
-# Get list of column names
-names(pas_apr10)
+setArchiveBaseUrl("https://airsensor.aqmd.gov/PurpleAir/v1/")
+pas_apr10 <- pas_load(datestamp="20220410")   # '20220410' = '4-10-2022'
 
 # Take a look at only PM2.5 data
 pm25 <- pas_apr10 %>%
@@ -209,15 +148,6 @@ mypat <- new_pat(pat_filename = "July_CFD12.rds",
 
 # Example PAT from AirSensor package
 pat <- AirSensor::example_pat
-
-# Open help documentation for example PAT
-help(example_pat)
-
-# Variables that do not change with time
-names(pat$meta)
-
-# Examine time series data
-print(pat$data, n=10)
 
 # Plot raw sensor data
 pat %>%
